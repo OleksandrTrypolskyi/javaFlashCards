98289ebfb54f38c6cdecbad39ae15826eb6bc4f7
TrypolskyiS <tripolskys@gmail.com>
2022-01-31 11:13:16 +0200

    Initialize Spring and Maven with dependencies

---
+++ src/main/java/com/oleksandrtrypolskyi/javaFlashCards/JavaFlashCardsApplication.java
@@ -0,0 +1,13 @@
+package com.oleksandrtrypolskyi.javaFlashCards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFlashCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFlashCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oleksandrtrypolskyi/javaFlashCards/JavaFlashCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oleksandrtrypolskyi.javaFlashCards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFlashCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
